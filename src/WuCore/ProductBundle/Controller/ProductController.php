dce4177ae2ef9f2af50e32b94a4b394d527a2d5e
Mohamed Racem Zouaghi <mr.zouaghi@gmail.com>
2013-02-10 12:27:22 +0100

    Correction de la déclaration des bundles et correction du routing

--- src/WuCore/ProductBundle/Controller/ProductController.php
+++ src/WuCore/ProductBundle/Controller/ProductController.php
@@ -4,38 +4,43 @@ namespace WuCore\ProductBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use WuCore\ProductBundle\Entity\Product;
 use WuCore\ProductBundle\Form\ProductType;
 
 /**
  * Product controller.
  *
+ * @Route("/product")
  */
-class ProductController extends Controller
-{
+class ProductController extends Controller {
+
     /**
      * Lists all Product entities.
      *
+     * @Route("/", name="product")
+     * @Template()
      */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
+    public function indexAction() {
+        $em = $this->getDoctrine()->getManager('products');
 
         $entities = $em->getRepository('WuCoreProductBundle:Product')->findAll();
 
-        return $this->render('WuCoreProductBundle:Product:index.html.twig', array(
+        return array(
             'entities' => $entities,
-        ));
+        );
     }
 
     /**
      * Finds and displays a Product entity.
      *
+     * @Route("/{id}/show", name="product_show")
+     * @Template()
      */
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
+    public function showAction($id) {
+        $em = $this->getDoctrine()->getManager('products');
 
         $entity = $em->getRepository('WuCoreProductBundle:Product')->find($id);
 
@@ -45,57 +50,62 @@ class ProductController extends Controller
 
         $deleteForm = $this->createDeleteForm($id);
 
-        return $this->render('WuCoreProductBundle:Product:show.html.twig', array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),        ));
+        return array(
+            'entity' => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
     }
 
     /**
      * Displays a form to create a new Product entity.
      *
+     * @Route("/new", name="product_new")
+     * @Template()
      */
-    public function newAction()
-    {
+    public function newAction() {
         $entity = new Product();
-        $form   = $this->createForm(new ProductType(), $entity);
+        $form = $this->createForm(new ProductType(), $entity);
 
-        return $this->render('WuCoreProductBundle:Product:new.html.twig', array(
+        return array(
             'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
+            'form' => $form->createView(),
+        );
     }
 
     /**
      * Creates a new Product entity.
      *
+     * @Route("/create", name="product_create")
+     * @Method("POST")
+     * @Template("WuCoreProductBundle:Product:new.html.twig")
      */
-    public function createAction(Request $request)
-    {
-        $entity  = new Product();
+    public function createAction(Request $request) {
+        $entity = new Product();
         $form = $this->createForm(new ProductType(), $entity);
         $form->bind($request);
 
         if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
+            $em = $this->getDoctrine()->getManager('products');
             $em->persist($entity);
             $em->flush();
 
             return $this->redirect($this->generateUrl('product_show', array('id' => $entity->getId())));
         }
 
-        return $this->render('WuCoreProductBundle:Product:new.html.twig', array(
+        return array(
             'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
+            'form' => $form->createView(),
+        );
     }
 
     /**
      * Displays a form to edit an existing Product entity.
      *
+     * @Route("/{id}/edit", name="product_edit")
+     * @Template()
      */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
+    public function editAction($id) {
+        $em = $this->getDoctrine()->getManager('products');
 
         $entity = $em->getRepository('WuCoreProductBundle:Product')->find($id);
 
@@ -106,20 +116,22 @@ class ProductController extends Controller
         $editForm = $this->createForm(new ProductType(), $entity);
         $deleteForm = $this->createDeleteForm($id);
 
-        return $this->render('WuCoreProductBundle:Product:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
+        return array(
+            'entity' => $entity,
+            'edit_form' => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
-        ));
+        );
     }
 
     /**
      * Edits an existing Product entity.
      *
+     * @Route("/{id}/update", name="product_update")
+     * @Method("POST")
+     * @Template("WuCoreProductBundle:Product:edit.html.twig")
      */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
+    public function updateAction(Request $request, $id) {
+        $em = $this->getDoctrine()->getManager('products');
 
         $entity = $em->getRepository('WuCoreProductBundle:Product')->find($id);
 
@@ -138,24 +150,25 @@ class ProductController extends Controller
             return $this->redirect($this->generateUrl('product_edit', array('id' => $id)));
         }
 
-        return $this->render('WuCoreProductBundle:Product:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
+        return array(
+            'entity' => $entity,
+            'edit_form' => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
-        ));
+        );
     }
 
     /**
      * Deletes a Product entity.
      *
+     * @Route("/{id}/delete", name="product_delete")
+     * @Method("POST")
      */
-    public function deleteAction(Request $request, $id)
-    {
+    public function deleteAction(Request $request, $id) {
         $form = $this->createDeleteForm($id);
         $form->bind($request);
 
         if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
+            $em = $this->getDoctrine()->getManager('products');
             $entity = $em->getRepository('WuCoreProductBundle:Product')->find($id);
 
             if (!$entity) {
@@ -169,11 +182,11 @@ class ProductController extends Controller
         return $this->redirect($this->generateUrl('product'));
     }
 
-    private function createDeleteForm($id)
-    {
+    private function createDeleteForm($id) {
         return $this->createFormBuilder(array('id' => $id))
-            ->add('id', 'hidden')
-            ->getForm()
+                        ->add('id', 'hidden')
+                        ->getForm()
         ;
     }
+
 }
